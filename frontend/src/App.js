1b8b4b61df8f100dceb4857d9d5b5def1ee7630d
hemmy-lola <hl.mathys@gmail.com>
2023-11-28 15:49:52 +0100

    header footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,5 @@
 import React from "react";
-// import LandingPage from "./LandingPage";
 import "./styles.css";
-// import MapPage from "./Components/page/MapPage";
-// import React, { useState, useEffect, useRef } from "react";
-// import axios from "axios";
-// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-// import MarkerClusterGroup from "react-leaflet-cluster";
-// import L from "leaflet";
-// import "leaflet/dist/leaflet.css";
-// import "leaflet-routing-machine";
-// import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
-// import Navbar from "./Components/Navbar";
-// import Footer from './Components/Footer';
 import LandingPage from "./page/LandingPage";
 import Navbar from "./Components/layout/navbar/Navbar"
 import Footer from './Components/layout/footer/Footer';
