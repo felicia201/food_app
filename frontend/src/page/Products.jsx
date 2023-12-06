1f23cada56d663896a4f6c184eda5d94baac7e7e
Joël Henri <ebosqua.edimo@gmail.com>
2023-12-07 00:17:28 +0100

    Style pages produits

--- frontend/src/page/Products.jsx
+++ frontend/src/page/Products.jsx
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from 'react'
 // import Image from "/asset/Hamburger.jpg"
 import "../Styles/App.scss"
 
+function InputRadio({ name, value }){
+    return(
+        <div className="choices">
+            <input type="radio" name={name} value={value} id={value} className="choice"/>
+            <label htmlFor={value}>{value}</label>
+        </div>
+        )
+}
 
 function SpecialOffer(){
     return (
@@ -9,94 +17,109 @@ function SpecialOffer(){
     )
 }
 
-function Filter(){
+function Filter({ filter, name, values }){
 
-    const [clicked, setClicked] = useState(false)
+    const [opened, setOpened] = useState(true)
 
-    function getUp(){
-        let a = document.querySelector(".options").clientHeight
-        // document.querySelector(".options").style.transform = `translateY(-${a}px)`
-        // document.querySelector(".options").style.visibility = `hidden`
-        console.dir(document.querySelector(".options"))
-        console.log(document.querySelector(".options").animationName)
+    // function getUp(){
+    //     let a = document.querySelector(".options").clientHeight
+    //     // document.querySelector(".options").style.transform = `translateY(-${a}px)`
+    //     // document.querySelector(".options").style.visibility = `hidden`
+    //     console.dir(document.querySelector(".options"))
+    //     console.log(document.querySelector(".options").animationName)
+    // }
+
+    function rotation(e){
+        setOpened(!opened)
+        e.target.classList.toggle("rotate")
+        // document.querySelector(".options").classList.toggle("hide")
     }
 
     return (
         <>
-            <div className="title-options"><h4>Evènements <button onClick={() => getUp()}>O</button></h4></div>
+            <div className="title-options"><h4>{filter}</h4><img src="/asset/arrow.svg" alt="" width="10px" height="10px" onClick={(e) => rotation(e)}/></div>
+            {
+            opened &&
             <div className="options">
-                <button>lorem</button>
-                <button>lorem</button>
-                <button>lorem</button>
-                <button>lorem</button>
-                <button>lorem</button>
+                {values.map((element,index) => 
+                    <InputRadio name={name} value={element} key={index} />
+                )}
             </div>
+            }
         </>
     )
 }
 
 function Filters(){
+    const prix = ["Inférieur à 10€", "Inférieur à 100€", "Inférieur à 250€", "Inférieur à 500€"]
+    const regime = ["Halal", "Végétarien", "Normal"]
+    const cuisine = ["Côtes de porc", "Emincé de poulet", "Soupe aux choux", "Poulet pané"]
     return (
         <div className="filter">
-            <Filter />
-            <Filter />
-            <Filter />
+            <Filter name="prix" filter="Prix" values={prix}/>
+            <Filter name="regime" filter="Régime" values={regime}/>
+            <Filter name="cuisine" filter="Cuisine" values={cuisine}/>
         </div>
         // <img src="./banner" alt="Banner" width="100vw" height="20vh"/>
     )
 }
 
-function Product(){
+function Product({ title, minimum, city, image }){
     return (
+    // <a href="/details">
+    //     <div className="product">
+    //         <img src="/asset/Hamburger.jpg" alt="Produit" width="250px" height="250px"/>
+    //         <div>
+    //             <div className="content">
+    //                 <div className="left">
+    //                     <span className="titre">Le coucher de soleil</span>
+    //                     <span>9 Avenue du vieux duc</span>
+    //                     <span className="min">Min. 10€</span>
+    //                 </div>
+    //                 <div className="right">
+    //                     <span className="opened"><div className="separator"></div> Ouvert</span>
+    //                     <button>Enregistrer</button>
+    //                 </div>
+    //             </div>
+    //             <div className="bott">
+    //                 <div className="menu-product">
+    //                     <span><div className="bullet"></div> Plat 1</span>
+    //                     <span><div className="bullet"></div> Plat 2</span>
+    //                     <span><div className="bullet"></div> Plat 3</span>
+    //                     <span><div className="bullet"></div> Plat 4</span>
+    //                 </div>
+    //                 <button>Découvrir</button>
+    //             </div>
+    //         </div>
+    //     </div>
+    // </a>
     <a href="/details">
         <div className="product">
-            <img src="/asset/Hamburger.jpg" alt="Produit" width="250px" height="250px"/>
-            <div>
-                <div className="content">
-                    <div className="left">
-                        <span className="titre">Le coucher de soleil</span>
-                        <span>9 Avenue du vieux duc</span>
-                        <span className="min">Min. 10€</span>
-                    </div>
-                    <div className="right">
-                        <span className="opened"><div className="separator"></div> Ouvert</span>
-                        <button>Enregistrer</button>
-                    </div>
-                </div>
-                <div className="bott">
-                    <div className="menu-product">
-                        <span><div className="bullet"></div> Plat 1</span>
-                        <span><div className="bullet"></div> Plat 2</span>
-                        <span><div className="bullet"></div> Plat 3</span>
-                        <span><div className="bullet"></div> Plat 4</span>
-                    </div>
-                    <button>Découvrir</button>
-                </div>
+            <div className="img" style={{backgroundImage: `url("${image}")`, backgroundSize: "cover", backgroundPosition:"center", backgroundRepeat: "no-repeat", width: "250px", height: "150px"}} ></div>
+            <span className="titre">{title}</span>
+            <div className="comp">
+                <span>Min. {minimum}</span>
+                <div className="bullet"></div>
+                <span>{city}</span>
             </div>
         </div>
     </a>
-//     <a href="/product/">
-//     <div className="product">
-//         <img src={Image.src} alt="Produit" width="250px" height="250px"/>
-//         <span className="titre">Le coucher de soleil</span>
-//         <span>Min. 10€</span>
-//     </div>
-//     </a>
     )
 }
 
 function AllProducts(){
     return (
+        // EN attendant de réaliser le fetch
         <div className="AllProducts">
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
+            <Product title="Le coucher de soleil" minimum="10€" city="Montreuil" image="/asset/Hamburger.jpg"/>
+            <Product title="Le coucher de soleil" minimum="10€" city="Montreuil" image="/asset/Hamburger.jpg"/>
+            <Product title="Le coucher de soleil" minimum="10€" city="Montreuil" image="/asset/Hamburger.jpg"/>
+            <Product title="Le coucher de soleil" minimum="10€" city="Montreuil" image="/asset/Hamburger.jpg"/>
+            <Product title="Le coucher de soleil" minimum="10€" city="Montreuil" image="/asset/Hamburger.jpg"/>
+            <Product title="Le coucher de soleil" minimum="10€" city="Montreuil" image="/asset/Hamburger.jpg"/>
+            <Product title="Le coucher de soleil" minimum="10€" city="Montreuil" image="/asset/Hamburger.jpg"/>
+            <Product title="Le coucher de soleil" minimum="10€" city="Montreuil" image="/asset/Hamburger.jpg"/>
+            <Product title="Le coucher de soleil" minimum="10€" city="Montreuil" image="/asset/Hamburger.jpg"/>
         </div>
     )
 }
--- frontend/src/page/ProductsDetails.jsx
+++ frontend/src/page/ProductsDetails.jsx
@@ -1,117 +1,176 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 // import Image from "/asset/Hamburger.jpg"
 // import Image2 from "/asset/Hamburger 2.jpg"
 // import Localisation from "/asset/localisateur.png"
 import "../Styles/App.scss"
 
-function Banner(){
+// function Banner(){
+//     return (
+//         <div className="banner">
+//             <img src="/asset/Hamburger.jpg" alt="Banner" id="banner"/>
+//         </div>
+//     )
+// }
+
+
+// function Note({ note }){
+//     return(
+//            <div className="note">
+//             <span>{note}</span>
+//            </div> 
+//     )
+// }
+
+// function Notes(){
+//     return(
+//         <>
+//            <Note note={1} /> 
+//            <Note note={2} /> 
+//            <Note note={3} /> 
+//            <Note note={4} /> 
+//            <Note note={5} /> 
+//         </>
+//     )
+// }
+
+function Details({ image, title, type, city, opening, closing, menu, min, max, description }){
     return (
-        <div className="banner">
-            <img src="/asset/Hamburger.jpg" alt="Banner" id="banner"/>
+        <div className="details">
+                {/* <div className="notes">
+                    <Notes />
+                </div> */}
+            <div className="top">
+                <div className="left">
+                    <img src={image} alt="Banner" id="details"/>
+                    
+                </div>
+                <div className="right">
+                    
+                    <div className="title">
+                        <p>{title}</p>
+                    </div>
+                    {/* <div className="opened">
+                        <div className="separator-info"></div><span>Ouvert</span>
+                    </div> */}
+                    <div className="infos">
+                        <span>{type}</span>
+                        <div className="separator-info"></div>
+                        <span>{city}</span>
+                        <div className="separator-info"></div>
+                        <span>{opening} - {closing}</span>
+                    </div>
+                    
+                    <div className="menu">
+                        <span>Aile de poulet</span>
+                        <div className="separator-menu"></div>
+                        <span>Cuisse de poulet</span>
+                        <div className="separator-menu"></div>
+                        <span>Pilon de poulet</span>
+                                    </div>
+                        <div className="price"><span>{min} - {max}</span>
+
+                        </div>
+                    </div>
+                    {/* <div className="register">
+
+                    </div> */}
+            </div>
+                <div className="description">
+                    <p>
+                        {description}
+                    </p>
+                </div>
         </div>
     )
 }
 
-
-function Note({ note }){
-    return(
-           <div className="note">
-            <span>{note}</span>
-           </div> 
+function Modalities({ place }){
+    return (
+        <div className="modalities" style={{backgroundImage: `url("/asset/Image2.jpg")`, backgroundSize: "cover", backgroundPosition:"center", backgroundRepeat: "no-repeat"}}>
+            <div>
+                <div className="top">
+                    <span><img src="/asset/localisateur.png" alt="alt" width="25px" height="25px"/> {place}</span>
+                    {/* <button>Enregistrer</button> */}
+                </div>
+                <div className="bottom">
+                    <button>Faire une réservation</button>
+                    <button>Effectuer une commande</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
-function Notes(){
+function Basket({ numberOfArticles, menu }){
+    const inTheBasket = menu.filter((element) => element.times !== 0)
+    // console.log(menu)
+    // console.log(inTheBasket)
+    return (
+        <div className="basket">
+            <div>
+                <div className="top">
+                    <img src="/asset/cart.svg" alt="alt" width="55px" height="55px"/>
+                </div>
+                <div className="bottom">
+                    <button>Afficher le panier({numberOfArticles})</button>
+                </div>
+            </div>
+            {
+            inTheBasket.length !== 0
+            &&
+            inTheBasket.map((element) =>
+            <><span>{element.title}</span><span>{element.price}</span></>)
+            }
+        </div>
+    )
+}
+
+function MenuItem({ image, title, ingredients, price, menuItemClicked, onMenuItemClicked, onAddItemToList, menu }){
+    
+    
+
+    return(
+    <div className="menuitem">
+        <div className="top" style={{backgroundImage: `url("${image}")`, backgroundSize: "cover", backgroundPosition:"center", backgroundRepeat: "no-repeat", width: "200px", height: "125px"}}>
+            
+        </div>
+        <div className="bottom">
+            <span className="title">{title}</span>
+            <span className="ingredients"><i>{ingredients}</i></span>
+            <span className="price">{price}</span>
+            <div onClick={onMenuItemClicked}><img src="/asset/plus.svg" alt="" width="30px" height ="30px"/></div>
+        </div>
+    </div>
+    )
+}
+
+function Menu({ onMenuItemClicked, onAddItemToList, menuItems, menuItemClicked }){
+    
     return(
         <>
-           <Note note={1} /> 
-           <Note note={2} /> 
-           <Note note={3} /> 
-           <Note note={4} /> 
-           <Note note={5} /> 
+        <p className="phrase-menu"><i>Découvrez le menu...</i></p>
+        <div className="menus">
+            {menuItems.map((element) =>
+            <MenuItem key={element.title} image={element.image} title={element.title} ingredients={element.ingredients} price={element.price} menuItemClicked={menuItemClicked} onMenuItemClicked={onMenuItemClicked} onAddItemToList={onAddItemToList} menu={menuItems}/>)}
+        </div>
         </>
     )
 }
 
-function Details(){
-    return (
-        <div className="details">
-            <div className="left">
-                <img src="/asset/Hamburger.jpg" alt="Banner" id="details"/>
-                
-                <div className="notes">
-                    <Notes />
-                </div>
-            </div>
-            <div className="right">
-                <div className="title">
-                    <p>LE COUCHER DE SOLEIL</p>
-                </div>
-                <div className="opened">
-                    <div className="separator-info"></div><span>Ouvert</span>
-                </div>
-                <div className="infos">
-                    <span>Thaï</span>
-                    <div className="separator-info"></div>
-                    <span>Paris - 15ème arr</span>
-                    <div className="separator-info"></div>
-                    <span>07 H 30 - 19 H 30</span>
-                </div>
-                
-                <div className="menu">
-                    <span>Aile de poulet</span>
-                    <div className="separator-menu"></div>
-                    <span>Cuisse de poulet</span>
-                    <div className="separator-menu"></div>
-                    <span>Pilon de poulet</span>
-                                </div>
-                    <div className="price"><span>20€ - 350€</span>
-
-                    </div>
-                    <div className="register">
-
-                    </div>
-                <div className="description">
-                    <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione saepe voluptate natus vel, exercitationem eveniet blanditiis rem porro, placeat, beatae in necessitatibus repellat itaque. Repellat possimus quisquam quas recusandae sint nemo facere nam voluptatem eveniet, reiciendis, aliquam et ab eum ratione perferendis numquam, quibusdam rem enim. Repudiandae dolor fugiat est.
-                    </p>
-                </div>
-            </div>
-        </div>
-    )
-}
-
-function Modalities(){
-    return (
-        <div className="modalities">
-            <div>
-                <div className="top">
-                    <span><img src="/asset/localisateur.png" alt="alt" width="25px" height="25px"/> 9 Avenue du Vieux Duc</span>
-                    <button>Enregistrer</button>
-                </div>
-                <div className="bottom">
-                    <button className="action">Prendre une réservation</button>
-                    <button className="action">Effectuer une commande</button>
-                </div>
-            </div>
-        </div>
-    )
-}
-
-function Comment(){
+function Comment({ name, date, note, content }){
     return (
         <div className="comment">
             <div className="top">
                 <div className="picture left"></div>
                 <div className="right">
                     <div className="left">
-                        <div className="name">Gauthier Théophile</div>
-                        <div className="day">22/10/2023</div>
+                        <div className="name">{name}</div>
+                        <div className="day">{date}</div>
                     </div>
-                    <div className="note-comment">4/5</div>
+                    <div className="note-comment">{note}/5</div>
                 </div>
             </div>
-            <p className="comment-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem delectus vero, nihil fuga, tempora nemo vitae, autem totam optio praesentium deleniti cupiditate aspernatur sed error id repellat ullam voluptatem quasi.</p>
+            <p className="comment-text">{content}</p>
         </div>
     )
 }
@@ -119,22 +178,22 @@ function Comment(){
 function Comments(){
     return (
         <div className="comments">
-            <Comment />
-            <Comment />
-            <Comment />
+            <Comment name="Gauthier Théophile" date="22/11/2023" note="4" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem delectus vero, nihil fuga, tempora nemo vitae, autem totam optio praesentium deleniti cupiditate aspernatur sed error id repellat ullam voluptatem quasi."/>
+            <Comment name="Gauthier Théophile" date="22/11/2023" note="4" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem delectus vero, nihil fuga, tempora nemo vitae, autem totam optio praesentium deleniti cupiditate aspernatur sed error id repellat ullam voluptatem quasi."/>
+            <Comment name="Gauthier Théophile" date="22/11/2023" note="4" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem delectus vero, nihil fuga, tempora nemo vitae, autem totam optio praesentium deleniti cupiditate aspernatur sed error id repellat ullam voluptatem quasi."/>
         </div>
     )
 }
 
-function Images(){
-    return (
-        <div className="places">
-            <img src="/asset/Hamburger 2.jpg" alt="Place" className="place"/>
-            <img src="/asset/Hamburger 2.jpg" alt="Place" className="place"/>
-            <img src="/asset/Hamburger 2.jpg" alt="Place" className="place"/>
-        </div>
-    )
-}
+// function Images(){
+//     return (
+//         <div className="places">
+//             <img src="/asset/Hamburger 2.jpg" alt="Place" className="place"/>
+//             <img src="/asset/Hamburger 2.jpg" alt="Place" className="place"/>
+//             <img src="/asset/Hamburger 2.jpg" alt="Place" className="place"/>
+//         </div>
+//     )
+// }
 
 function Reinsurance(){
     return (
@@ -144,14 +203,39 @@ function Reinsurance(){
 }
 
 export default function DetailsOfProducts() {
+    const [menuItemClicked, setMenuItemClicked] = useState(0)
+
+    const [menuList, setMenuList] = useState(
+        [
+            {image: "/asset/Hamburger 2.jpg", title: "Hamburger", ingredients: "Steak, fromage, tomate, salade, tranches de pain, cornichons", price: "17€"},
+            {image: "/asset/Hamburger 2.jpg", title: "Hamburger 2", ingredients: "Steak, fromage, tomate, salade, tranches de pain, cornichons", price: "17€"},
+            {image: "/asset/Hamburger 2.jpg", title: "Hamburger 3", ingredients: "Steak, fromage, tomate, salade, tranches de pain, cornichons", price: "17€"},
+            {image: "/asset/Hamburger 2.jpg", title: "Hamburger 4", ingredients: "Steak, fromage, tomate, salade, tranches de pain, cornichons", price: "17€"}
+        ])
+    const menu = menuList.map((element) => {return {...element, times: 0}} )
+    
+    function handleMenuItemClick(){
+        setMenuItemClicked(m => m + 1)
+    }
+
+    function handleAddItemToList(menu, title){
+        setMenuList(menu.filter((element) => {
+            if(element.title === title){
+                return {...element, times: element.times + 1}
+            }
+            return element
+        }))
+        // console.log(title)
+    }
   return (
     <>
         <div className="detailsofproducts">
-            <Banner />
-            <Details />
-            <Modalities />
+            <Details image="/asset/Hamburger.jpg" title="LE COUCHER DE SOLEIL" type="Thaï" city="Paris - 15ème arr." opening="07 H 30" closing="19 H 30" menu={["Aile de poulet", "Cuisse de poulet", "Pilon de poulet"]} min="20€" max="350€" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione saepe voluptate natus vel, exercitationem eveniet blanditiis rem porro, placeat, beatae in necessitatibus repellat itaque. Repellat possimus quisquam quas recusandae sint nemo facere nam voluptatem eveniet, reiciendis, aliquam et ab eum ratione perferendis numquam, quibusdam rem enim. Repudiandae dolor fugiat est."/>
+            <Modalities place="9 Avenue du Vieux Duc"/>
+            <Basket numberOfArticles={menuItemClicked} menu={menu}/>
+            <Menu onMenuItemClicked={handleMenuItemClick} onAddItemToList={handleAddItemToList} menuItems={menu} menuItemClicked={menuItemClicked}/>
             <Comments />
-            <Images />
+            {/* <Images /> */}
             <Reinsurance />
         </div>
     </>
